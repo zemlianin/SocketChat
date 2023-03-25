ae589d95670dc17457088edada5831417559b1b7
zemlianin <detulie@mail.ru>
2023-03-25 11:58:19 +0300

    auto-reload-msg

--- src/main/java/org/example/CapitalizeClient.java
+++ src/main/java/org/example/CapitalizeClient.java
@@ -5,8 +5,15 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class CapitalizeClient {
+    private ExecutorService executor
+            = Executors.newSingleThreadExecutor();
+
     public static void main(String[] args) throws Exception {
         try (var socket = new Socket("localhost", 59898)) {
             System.out.println("Enter lines of text then Ctrl+D or Ctrl+C to quit");
@@ -14,10 +21,26 @@ public class CapitalizeClient {
             var scanner = new Scanner(System.in);
             var in = new Scanner(socket.getInputStream());
             var out = new PrintWriter(socket.getOutputStream(), true);
+            Future<Integer> future = new Notificator().notificate(in);
             while (scanner.hasNextLine()) {
-                 out.println(scanner.nextLine());
-                 System.out.println(in.nextLine());
+                out.println(scanner.nextLine());
             }
         }
     }
+
+    public static class Notificator {
+
+        private ExecutorService executor
+                = Executors.newSingleThreadExecutor();
+
+        public Future<Integer> notificate(Scanner in) {
+            return executor.submit(() -> {
+                while (true) {
+                    System.out.println(in.nextLine());
+                    Thread.sleep(100);
+                }
+            });
+        }
+    }
+
 }
